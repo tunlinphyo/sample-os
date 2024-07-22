cee30470c4783110b538635b6670d1ff9820ca39
tunlinphyo <uniqueof1990@gmail.com>
2024-07-23 08:00:44 +0900

    update

--- src/apps/phone/pages/contacts.page.ts
+++ src/apps/phone/pages/contacts.page.ts
@@ -45,7 +45,9 @@ export class ContactsPage extends Page {
         const scrollArea = this.createScrollArea();
         const noteList = this.createElement('ul', ['titleList', 'contactList']);
         list.forEach(item => {
-            const contactName = this.createElement('li', ['titleItem', 'textLarge']);
+            const contactName = this.createElement('li', ['titleItem', 'textLarge'], { 
+                'data-name': `${item.firstName[0]}${item.lastName[0]}` 
+            });
             if (item.isBlocked) contactName.classList.add('blocked');
             contactName.textContent = `${item.firstName} ${item.lastName}`;
             this.addEventListener('click', () => {
